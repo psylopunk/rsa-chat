e91673b7996dbe957ce143f79b2e4b5d52a2f13e
psylopunk <psylopunk@users.noreply.github.com>
2020-05-22 02:56:23 +0300

    update 0.1.2

--- client/app.py
+++ client/app.py
@@ -1,4 +1,5 @@
 from PyQt5 import QtWidgets, uic
+import threading
 import websocket
 import rsa
 import design_app
@@ -7,7 +8,32 @@ import sys
 import json
 import time
 
-HOST = "localhost:9200"
+HOST = "192.236.179.125:9200"
+
+def on_message(ws, message):
+    data = json.loads(message)
+    print("[WS] < %s" % (data))
+    action = data["action"]
+    if action == "get_clients":
+        crypted_messages = []
+        message = window.inputMessage.text()
+        for pubkey in data["data"]["clients"]: crypted_messages.append(rsa.encrypt(message.encode("utf8"), rsa.PublicKey.load_pkcs1(bytes(bytearray.fromhex(pubkey)))).hex())
+        window.ws.send(json.dumps({"action": "send_message", "box": window.box, "message": crypted_messages}))
+    elif action == "receive_message":
+        for message in data["data"]:
+            try:
+                decrypted = rsa.decrypt(bytes(bytearray.fromhex(message)), window.privateKey).decode("utf8")
+                window.textBrowser.setText(window.textBrowser.toPlainText() + ("[%s]: %s\n" % (data["login"], decrypted)))
+            except: pass
+
+def on_error(ws, error):
+    print("Error websockets")
+
+def on_close(ws):
+    print("[WS] Closed connection")
+    window.connect_ws()
+    window.ws.send(json.dumps({"login": self.name, "box": self.box, "pubkey": self.rawPublicKey}))
+
 
 class ConnectGui(QtWidgets.QMainWindow, design_connect.Ui_MainWindow):
     def __init__(self):
@@ -26,34 +52,32 @@ class AppGui(QtWidgets.QMainWindow, design_app.Ui_MainWindow):
 
         self.publicKey, self.privateKey = rsa.newkeys(512)
         self.rawPublicKey = self.publicKey.save_pkcs1("PEM").hex()
+        print(self.privateKey.save_pkcs1("PEM").hex())
 
         self.btnSend.clicked.connect(self.send_message)
+
+    def connect_ws(self):
+        websocket.enableTrace(True)
+        self.ws = websocket.WebSocketApp("ws://%s/" % (HOST), on_message = on_message, on_error = on_error, on_close = on_close)
+        threading.Thread(target=self.ws.run_forever, args=[]).start()
 
     def connectBox(self):
         self.box = self.connectGui.inputBox.text()
         self.name = self.connectGui.inputName.text()
         print("Box: %s\nName: %s" % (self.box, self.name))
         self.connectGui.close()
-        self.ws = websocket.create_connection("ws://%s/" % HOST)
+        self.connect_ws()
+        time.sleep(0.1)
         self.ws.send(json.dumps({"login": self.name, "box": self.box, "pubkey": self.rawPublicKey}))
-        res = json.loads(self.ws.recv())
         # TODO except if not connect
-        print(res)
 
     def send_message(self):
         self.ws.send(json.dumps({"action": "get_clients", "box": self.box}))
-        res = json.loads(self.ws.recv())
 
-        crypted_messages = []
-        message = self.inputMessage.text()
-
-        for pubkey in res["data"]["clients"]: crypted_messages.append(rsa.encrypt(message.encode("utf8"), pubkey).hex())
-
-        self.ws.send(json.dumps({"action": "send_message", "box": self.box, "message": crypted_messages}))
-
-        self.textBrowser.setText(self.inputMessage.text())
+        # self.textBrowser.setText(self.inputMessage.text())
 
 def main():
+    global window
     app = QtWidgets.QApplication(sys.argv)
     window = AppGui()
     window.show()
--- server/server.py
+++ server/server.py
@@ -13,7 +13,7 @@ async def websocket_handler(websocket, path):
     if box not in boxes: boxes[box] = {"clients": []}
     if "pubkey" not in data: return await websocket.send(json.dumps({"success": False, "err": "Pubkey not specified"}))
     boxes[box]["clients"].append({"login": login, "pubkey": data["pubkey"], "ws": websocket})
-    await websocket.send(json.dumps({"success": True, "err": ""}))
+    await websocket.send(json.dumps({"action": "connect", "success": True, "err": ""}))
     print("Connected client [%s]" % login)
     while True:
         data = json.loads(await websocket.recv())
@@ -22,10 +22,14 @@ async def websocket_handler(websocket, path):
         if action == "get_clients":
             if "box" not in data: await websocket.send(json.dumps({"success": False, "err": "Box not specified"})); continue
             if data["box"] not in boxes: boxes[box] = {"clients": []}
-            await websocket.send(json.dumps({"success": True, "err": "", "data": {"clients": [client["pubkey"] for client in boxes[data["box"]]["clients"]]}}))
+            await websocket.send(json.dumps({"action": "get_clients", "success": True, "err": "", "data": {"clients": [client["pubkey"] for client in boxes[data["box"]]["clients"]]}}))
+        elif action == "send_message":
+            if "box" not in data: await websocket.send(json.dumps({"success": False, "err": "Box not specified"})); continue
+            if data["box"] not in boxes: boxes[box] = {"clients": []}
+            for client in boxes[data["box"]]["clients"]:
+                await client["ws"].send(json.dumps({"action": "receive_message", "login": login, "data": data["message"]}))
 
-
-        return await websocket.send(json.dumps({"success": False, "err": "Unknown error"}))
+        else: await websocket.send(json.dumps({"action": "unknown", "success": False, "err": "Unknown error"}))
 
 
 
